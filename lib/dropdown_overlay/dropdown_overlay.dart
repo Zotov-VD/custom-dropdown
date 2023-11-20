b771a3b2df9a4251b5f6254b4893157be3a86f5f
Vladimir <45392524+Zotov-VD@users.noreply.github.com>
2023-11-20 20:18:09 +0300

    Update dropdown_overlay.dart

--- lib/dropdown_overlay/dropdown_overlay.dart
+++ lib/dropdown_overlay/dropdown_overlay.dart
@@ -7,7 +7,7 @@ const _defaultOverlayIconUp = Icon(
 );
 
 const _headerPadding = EdgeInsets.all(16.0);
-const _overlayOuterPadding = EdgeInsets.only(bottom: 12, left: 12, right: 12);
+const _overlayOuterPadding = EdgeInsets.zero;
 const _overlayShadowOffset = Offset(0, 6);
 const _listItemPadding = EdgeInsets.symmetric(vertical: 12, horizontal: 16);
 
